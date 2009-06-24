360e812760ea71b655bed6b636fbd3f564b0a30f
Xavi <xavi.rmz@gmail.com>
2009-06-23 21:40:25 -0400

    Added a couple of project pages

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -19,7 +19,9 @@ class Boot {
     val entries = Menu(Loc("Home", List("index"), "Home")) ::
                   Menu(Loc("Visual Sort", List("visual-sort"), "Visual Sort")) ::
                   Menu(Loc("Drag Shapes", List("drag-shapes", "index"), "Drag Shapes"),
+                      Menu(Loc("Comet Drag", List("drag-shapes", "comet-drag"), "Comet Drag")),
                       Menu(Loc("Get the goal", List("drag-shapes", "get-the-goal"), "Get the goal")),
+                      Menu(Loc("Rotate Image", List("drag-shapes", "rotate-image"), "Rotate Image")),
                       Menu(Loc("Find the Slant", List("drag-shapes", "slanted-box"), "Find the slanted box"))) ::
                   Menu(Loc("Key Remapper", List("key-remapper"), "Key Remapper")) ::
                   Menu(Loc("tv-downloader", List("tv-schedule-downloader"), "TV Schedule Downloader")) ::
--- src/main/scala/net/lift/theXavi/snippet/HelloWorld.scala
+++
@@ -1,6 +0,0 @@
-package net.lift.theXavi.snippet
-
-class HelloWorld {
-  def howdy = <span>Welcome to theXavi at {new _root_.java.util.Date}</span>
-}
-
