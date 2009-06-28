55c942be35a27b3ebf7f9b78bdc3e3f5783d115a
Xavi <xavi.rmz@gmail.com>
2009-06-27 20:44:58 -0400

    Adding a customized 404 page.

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -14,20 +14,26 @@ class Boot {
   def boot {
     // where to search snippet
     LiftRules.addToPackages("net.lift.theXavi")
-        
+    
+    LiftRules.uriNotFound.prepend{
+      case (req, _) => PermRedirectResponse("404", req)
+    }
+    
     // Build SiteMap
-    val entries = Menu(Loc("Home", List("index"), "Home")) ::
-                  Menu(Loc("Visual Sort", List("visual-sort"), "Visual Sort")) ::
-                  Menu(Loc("Drag Shapes", List("drag-shapes", "index"), "Drag Shapes"),
-                      Menu(Loc("Comet Drag", List("drag-shapes", "comet-drag"), "Comet Drag")),
-                      Menu(Loc("Get the goal", List("drag-shapes", "get-the-goal"), "Get the goal")),
-                      Menu(Loc("Rotate Image", List("drag-shapes", "rotate-image"), "Rotate Image")),
-                      Menu(Loc("Find the Slant", List("drag-shapes", "slanted-box"), "Find the slanted box"))) ::
-                  Menu(Loc("Key Remapper", List("key-remapper"), "Key Remapper")) ::
-                  Menu(Loc("tv-downloader", List("tv-schedule-downloader"), "TV Schedule Downloader")) ::
-                  Menu(Loc("About Me", List("about-me"), "About Me")) ::
-                  Nil
-    LiftRules.setSiteMap(SiteMap(entries:_*))
+    val pages = Menu(Loc("Home", List("index"), "Home")) ::
+                Menu(Loc("Visual Sort", List("visual-sort"), "Visual Sort")) ::
+                Menu(Loc("Drag Shapes", List("drag-shapes", "index"), "Drag Shapes"),
+                    Menu(Loc("Comet Drag", List("drag-shapes", "comet-drag"), "Comet Drag")),
+                    Menu(Loc("Get the goal", List("drag-shapes", "get-the-goal"), "Get the goal")),
+                    Menu(Loc("Rotate Image", List("drag-shapes", "rotate-image"), "Rotate Image")),
+                    Menu(Loc("Find the Slant", List("drag-shapes", "slanted-box"), "Find the slanted box"))) ::
+                Menu(Loc("Key Remapper", List("key-remapper"), "Key Remapper")) ::
+                Menu(Loc("tv-downloader", List("tv-schedule-downloader"), "TV Schedule Downloader")) ::
+                Menu(Loc("About Me", List("about-me"), "About Me")) ::
+                Menu(Loc("404", List("404"), "404", Hidden)) ::
+                Nil
+    
+    LiftRules.setSiteMap(SiteMap(pages:_*))
   }
 }
 
