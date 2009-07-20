bf1a9c98ee4bee8d08feeb08bb5762508181c730
Xavi <xavi.rmz@gmail.com>
2009-07-20 05:06:52 -0400

    Made comet drag into a magnetic poetry app!

--- src/main/scala/net/lift/theXavi/comet/CometDragDisplay.scala
+++ src/main/scala/net/lift/theXavi/comet/CometDragDisplay.scala
@@ -12,22 +12,32 @@ import scala.actors.Actor._
 import scala.Nil
 
 case class BroadcastPlaces(senderId: String)
-case class ShapePlaces(senderId: String, places: Map[String, (Int, Int)])
+case class ShapePlaces(senderId: String, places: Map[String, (Int, Int, String)])
 case class MoveShapes(senderId: String, moves: Map[String, List[List[Number]]])
 case class AddListener(listener: Actor)
 
 object ShapeTracker extends Actor {
-  private var shapes = Map((1 to 5).map(x => ("shape"+x, (0,0))):_*)
+  private val words = List("Senior", "Chief", "Vice", "Lead", "Director", "Manager", "Principle", "Executive",
+                           "Officer", "Official", "Secretary", "Treasurer", "Agent", "Foreman", "President",
+                           "Chairman", "Visionary", "Supervisor", "Superintendent", "Head", "General", "Partner",
+                           "of", "of", "of", "of", "Information", "Financial", "Operating", "Legal", "Technical",
+                           "Business", "Administrator", "Administration", "Marketing", "Relations", "Performance",
+                           "Strategy", "Synergy", "Professional", "Liaison", "Consultant", "Specialist", "Operations",
+                           "Engineer", "Evangelist", "and", "and", "Security", "Creative")
+  private val rand = new java.util.Random
+  private var shapes = Map((0 to 49).map(x => ("shape"+x, (rand.nextInt(430), rand.nextInt(480), words(x)))):_*)
   private var listeners: List[Actor] = Nil
-  
+
   this.start
-   
+
   def act = loop {
     react {
       case BroadcastPlaces(senderId) =>
         listeners.foreach(_ ! ShapePlaces(senderId, shapes))
       case MoveShapes(senderId, moves) =>
-        for((name, posList) <- moves) { shapes = (shapes(name) = (posList.last(0).intValue, posList.last(1).intValue)) }
+        for((name, posList) <- moves) {
+          shapes = (shapes(name) = (posList.last(0).intValue, posList.last(1).intValue, shapes(name)._3))
+        }
         listeners.foreach(_ ! MoveShapes(senderId, moves))
       case AddListener(listener) =>
         listeners ::= listener
@@ -40,16 +50,16 @@ class CometDragDisplay extends CometActor {
   override def localSetup() {
     ShapeTracker ! AddListener(this)
   }
-  
+
   override def render =
     Script(Function("moveShape", List("points"), this.jsonCall("move", JsRaw("points"))) &
            Function("getPlacements", Nil, this.jsonCall("places")))
-    
+
   override def highPriority() = {
     case ShapePlaces(senderId, places) =>
       if(senderId != this.uniqueId) { continue }
 
-      val jsPlaces = JsObj(places.toSeq.map(x => (x._1, JsObj("x" -> x._2._1, "y" -> x._2._2))):_*)
+      val jsPlaces = JsObj(places.toSeq.map(x => (x._1, JsObj("x" -> x._2._1, "y" -> x._2._2, "word" -> x._2._3))):_*)
       partialUpdate(Call("receivePlacements", jsPlaces))
     case MoveShapes(senderId, moves) =>
       if(senderId == this.uniqueId) { continue }
@@ -59,7 +69,7 @@ class CometDragDisplay extends CometActor {
                       => JsObj("x" -> pos(0).intValue, "y" -> pos(1).intValue, "time" -> pos(2).longValue)):_*))):_*)
       partialUpdate(Call("receiveUpdate", jsMoves))
   }
-  
+
   override def handleJson(in: Any): JsCmd = in match {
     case JsonCmd("move", _, moves: Map[String, List[List[Number]]], _) =>
       ShapeTracker ! MoveShapes(this.uniqueId, moves)
