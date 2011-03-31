ac58fc7a1facd66f40b5ac296f012a47baabfe4a
Xavi <xavi.rmz@gmail.com>
2011-03-30 23:46:31 -0400

    Integrating new beeline library

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
+var http = require("http");
 var url = require("url");
 var fs = require("fs");
 var bind = require("./libraries/bind-js");
 var markdown = require("./libraries/markdown-js/lib/markdown");
-var srv = require("./libraries/xavlib/simple-router");
 var chn = require("./libraries/xavlib/channel");
+var bee = require("./libraries/beeline");
 
 process.title = "node-xavi.co";
 
 var DefaultBindHandler = (function() {
+    var bindLink = (function() {
+        var template = { "selected": "<span>(:name:)</span>",
+                         "not-selected": "<a href='(:url:)'>(:name:)</a>" };
+    
+        return function(callback, val, context) {
+            val = JSON.parse(val);
+        
+            if(context.id === val.id) { bind.to(template["selected"], val, callback); }
+            else { bind.to(template["not-selected"], val, callback); }
+        };
+    })();
+    
     function handler(context, req, res) {
         bind.toFile("./content/templates/default.html", context, function(data) {
             res.writeHead(200, { "Conent-Length": data.length,
@@ -34,71 +47,58 @@ var DefaultBindHandler = (function() {
     };
 })();
 
-var bindLink = (function() {
-    var template = { "selected": "<span>(:name:)</span>",
-                     "not-selected": "<a href='(:url:)'>(:name:)</a>" };
+var router = bee.line({
+    "`404`": DefaultBindHandler("./content/404.html"),
     
-    return function(callback, val, context) {
-        val = JSON.parse(val);
-        
-        if(context.id === val.id) { bind.to(template["selected"], val, callback); }
-        else { bind.to(template["not-selected"], val, callback); }
-    };
-})();
+    "/robots.txt": bee.staticFileHandler("./content/robots.txt", "text/plain"),
+    "/client.js": bee.staticFileHandler("./libraries/xavlib/channel/client.js", "application/x-javascript"),
+    
+    "r`^/pics/(.*)$`": bee.staticDirHandler("./content/pics/", { "gif": "image/gif", "png": "image/png",
+                                                                 "jpg": "image/jpeg", "jpeg": "image/jpeg" }),
+    
+    "/ /index.html": DefaultBindHandler("./content/index.html", "home"),
+    "/about-me": DefaultBindHandler("./content/about-me.html", "about-me"),
+    "/resume": DefaultBindHandler("./content/resume.html", "resume"),
+    "/key-remapper": DefaultBindHandler("./content/key-remapper.html", "key-map"),
+    "/magnetic-poetry": DefaultBindHandler("./content/magnetic-poetry.html", "mag-poetry"),
+    "/tv-schedule-downloader": DefaultBindHandler("./content/tv-schedule-downloader.html", "tv-download"),
+    "/visual-sort": DefaultBindHandler("./content/visual-sort.html", "visual-sort"),
+    
+    "/interview": bee.staticFileHandler("./content/interview.html", "text/html"),
+    "/js-idiom /js-idioms": bee.staticFileHandler("./content/js-idioms.html", "text/html"),
+    
+    "/articles /articles/ /articles/index.html": DefaultBindHandler("./content/articles/index.html", "articles"),
+    "/articles/operation-is-not-supported-code-9":
+        DefaultBindHandler("./content/articles/operation-is-not-supported-code-9.html"),
+    "/articles/cool-chrome-eye-candy": DefaultBindHandler("./content/articles/cool-chrome-eye-candy.html"),
+    "/articles/qunit-html-template": DefaultBindHandler("./content/articles/qunit-html-template.html"),
+    "/articles/fun-with-tostring-in-javascript":
+        DefaultBindHandler("./content/articles/fun-with-tostring-in-javascript.html"),
+    "/articles/fun-with-valueof-in-javascript":
+        DefaultBindHandler("./content/articles/fun-with-valueof-in-javascript.html"),
+    "/articles/jquery-ui-css-themes-hosted-on-cdn":
+        DefaultBindHandler("./content/articles/jquery-ui-css-themes-hosted-on-cdn.html"),
+    "/articles/function-caller-chromes-leaky-abstraction":
+        DefaultBindHandler("./content/articles/function-caller-chromes-leaky-abstraction.html"),
+    "/articles/trouble-with-touch-events-jquery":
+        DefaultBindHandler("./content/articles/trouble-with-touch-events-jquery.html"),
+    "/articles/get-around-cross-domain-restrictions":
+        DefaultBindHandler("./content/articles/get-around-cross-domain-restrictions.html"),
+    "/articles/running-rails-app-from-github":
+        DefaultBindHandler("./content/articles/running-rails-app-from-github.html"),
+    
+    "/drag-shapes /drag-shapes/ /drag-shapes/index.html":
+        DefaultBindHandler("./content/drag-shapes/index.html", "drag-shapes"),
+    "/drag-shapes/get-the-goal": DefaultBindHandler("./content/drag-shapes/get-the-goal.html"),
+    "/drag-shapes/rotate-image": DefaultBindHandler("./content/drag-shapes/rotate-image.html"),
+    "/drag-shapes/slanted-box": DefaultBindHandler("./content/drag-shapes/slanted-box.html"),
+    "/drag-shapes/comet-drag":
+        DefaultBindHandler("./content/drag-shapes/comet-drag.html", require("./apps/comet-drag").start(chn))
+});
 
-srv.error = DefaultBindHandler("./content/404.html");
+var server = http.createServer(router);
 
-srv.patterns.push(srv.staticDirHandler("/pics/", "./content/pics/", "png", "image/png"));
-srv.patterns.push(srv.staticDirHandler("/pics/", "./content/pics/", "jpg", "image/jpeg"));
-srv.patterns.push(srv.staticDirHandler("/pics/", "./content/pics/", "gif", "image/gif"));
+//chn.start(server);
 
-srv.urls["/robots.txt"] = srv.staticFileHandler("./content/robots.txt", "text/plain");
-
-srv.urls["/client.js"] = srv.staticFileHandler("./libraries/xavlib/channel/client.js", "application/x-javascript");
-
-srv.urls["/"] = srv.urls["/index.html"] = DefaultBindHandler("./content/index.html", "home");
-srv.urls["/about-me"] = DefaultBindHandler("./content/about-me.html", "about-me");
-srv.urls["/resume"] = DefaultBindHandler("./content/resume.html", "resume");
-srv.urls["/key-remapper"] = DefaultBindHandler("./content/key-remapper.html", "key-map");
-srv.urls["/magnetic-poetry"] = DefaultBindHandler("./content/magnetic-poetry.html", "mag-poetry");
-srv.urls["/tv-schedule-downloader"] = DefaultBindHandler("./content/tv-schedule-downloader.html", "tv-download");
-srv.urls["/visual-sort"] = DefaultBindHandler("./content/visual-sort.html", "visual-sort");
-
-srv.urls["/interview"] = srv.staticFileHandler("./content/interview.html", "text/html");
-srv.urls["/js-idiom"] = srv.urls["/js-idioms"] = srv.staticFileHandler("./content/js-idioms.html", "text/html");
-
-srv.urls["/articles"] = 
-srv.urls["/articles/"] = 
-srv.urls["/articles/index.html"] = DefaultBindHandler("./content/articles/index.html", "articles");
-srv.urls["/articles/operation-is-not-supported-code-9"] =
-    DefaultBindHandler("./content/articles/operation-is-not-supported-code-9.html");
-srv.urls["/articles/cool-chrome-eye-candy"] = DefaultBindHandler("./content/articles/cool-chrome-eye-candy.html");
-srv.urls["/articles/qunit-html-template"] = DefaultBindHandler("./content/articles/qunit-html-template.html");
-srv.urls["/articles/fun-with-tostring-in-javascript"] =
-    DefaultBindHandler("./content/articles/fun-with-tostring-in-javascript.html");
-srv.urls["/articles/fun-with-valueof-in-javascript"] =
-    DefaultBindHandler("./content/articles/fun-with-valueof-in-javascript.html");
-srv.urls["/articles/jquery-ui-css-themes-hosted-on-cdn"] =
-    DefaultBindHandler("./content/articles/jquery-ui-css-themes-hosted-on-cdn.html");
-srv.urls["/articles/function-caller-chromes-leaky-abstraction"] =
-    DefaultBindHandler("./content/articles/function-caller-chromes-leaky-abstraction.html");
-srv.urls["/articles/trouble-with-touch-events-jquery"] = 
-    DefaultBindHandler("./content/articles/trouble-with-touch-events-jquery.html");
-srv.urls["/articles/get-around-cross-domain-restrictions"] = 
-    DefaultBindHandler("./content/articles/get-around-cross-domain-restrictions.html");
-srv.urls["/articles/running-rails-app-from-github"] = 
-    DefaultBindHandler("./content/articles/running-rails-app-from-github.html");
-
-srv.urls["/drag-shapes"] =
-srv.urls["/drag-shapes/"] =
-srv.urls["/drag-shapes/index.html"] = DefaultBindHandler("./content/drag-shapes/index.html", "drag-shapes");
-srv.urls["/drag-shapes/get-the-goal"] = DefaultBindHandler("./content/drag-shapes/get-the-goal.html");
-srv.urls["/drag-shapes/rotate-image"] = DefaultBindHandler("./content/drag-shapes/rotate-image.html");
-srv.urls["/drag-shapes/slanted-box"] = DefaultBindHandler("./content/drag-shapes/slanted-box.html");
-srv.urls["/drag-shapes/comet-drag"] =
-    DefaultBindHandler("./content/drag-shapes/comet-drag.html", require("./apps/comet-drag").start(chn));
-
-chn.start(srv);
-
-srv.server.listen(8004);
+server.listen(8004);
 console.log("Serving xavi.co on port 8004");
